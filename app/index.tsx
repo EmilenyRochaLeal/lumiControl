b26b8a0197de69b7df30e8452064c81c77c55b58
EmilenyRochaLeal <emilenyleal20@gmail.com>
2025-01-22 22:45:41 -0300

    style: estilização da page home

--- app/index.tsx
+++ app/index.tsx
@@ -10,31 +10,55 @@ import  Light  from '@/components/Light';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Brilho Atual</Text>
-      <Link href={"/notificacao"}>Ir para as notificações</Link>
-      <Link href={"/configuracao"}>Ir para as configurações</Link>
-      <Light/>
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem vindo(a)</Text>
+      <Text style={styles.subTitle}>LuminControl</Text>
+      <View style={styles.linkContainer}>
+        <Link href={"/notificacao"} style={styles.link}>Ir para as notificações</Link>
+        <Link href={"/configuracao"} style={styles.link}>Ir para as configurações</Link>
+      </View>
+      {/* <Light/> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // titleContainer: {
-  //   flexDirection: 'column',
-  //   alignItems: 'center',
-  //   justifyContent: "center",
-  //   gap: 8,
-  // },
-  // stepContainer: {
-  //   gap: 8,
-  //   marginBottom: 8,
-  // },
-  // reactLogo: {
-  //   height: 178,
-  //   width: 290,
-  //   bottom: 0,
-  //   left: 0,
-  //   position: 'absolute',
-  // },
+  container: {
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center", 
+    backgroundColor: "#f5f5f5", 
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold", 
+    textAlign: "center", 
+    // marginBottom: 20, 
+    color: "#333", 
+  },
+  subTitle: {
+    fontSize: 34,
+    fontWeight: 100
+  },
+  linkContainer: {
+    flexDirection: "row", 
+    justifyContent: "space-around", 
+    width: "100%", 
+    paddingHorizontal: 20, 
+    position: "absolute", 
+    bottom: 20, 
+  },
+  link: {
+    fontSize: 10, 
+    fontWeight: 'bold',
+    color: "#fff", 
+    textDecorationLine: "underline", 
+    marginVertical: 10, 
+    backgroundColor: "#007BFF",
+    textAlign: "center", 
+    paddingVertical: 20, 
+    paddingHorizontal: 23, 
+    borderRadius: 12, 
+    overflow: "hidden",
+  },
 });
